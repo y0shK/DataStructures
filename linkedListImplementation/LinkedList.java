bcf527851034d80ffa427a70d600c5ba5ec2d64b
Yash Karandikar <ynkarandikar@gmail.com>
2021-01-29 12:15:37 -0800

    implement prepend, insert, TODO delete

--- linkedListImplementation/LinkedList.java
+++ linkedListImplementation/LinkedList.java
@@ -69,4 +69,85 @@ public class LinkedList {
 	}
 	
 	
+	// create a prepend function - only attach as the first element in the linked list
+	// prepend is opposite of append
+	// check to see if the node after the current one is the head
+		// if it is, then add the node at the very beginning
+	
+	public void prepend(int data) {
+		
+		//Node current = head;
+		Node newnode = new Node(data);
+		
+		Node temp = head;
+		head = newnode; // put it at the very beginning such that newnode becomes the head
+		newnode.next = temp; // all other connections in the linked list stay the same
+		
+	}
+	
+	// insertTo is a more generic append function which can put the new node in any position
+	// track which position we're at using a counter
+	// when we're at the specific index we want to put the new node in, put the new node in
+	// the rest of the links are unchanged
+	
+	// the index provided should be zero-indexed - 0 is 1st element, etc.
+	public void insertTo(int data, int index) {
+		
+		Node current = head;
+		int counter = 0;
+		
+		while (current.next != null) {
+			counter++;
+			
+			if (counter == index) {
+				Node temp = current.next;
+				current.next = new Node(data);
+				current.next.next = temp;
+			}
+			current = current.next; // remember to move through the nodes to eventually finish the while loop
+			
+		}
+		
+	}
+	
+	// delete will work similarly to insert
+	// track the position we're at using a counter
+	// when we're at the position, delete the node/reassign the references to patch holes
+	public void delete(int index) {
+		
+		Node current = head;
+		int counter = 0;
+		
+		// 3 cases
+		// 1st case: delete first index (0)
+		// TODO fix this case
+		if (index == 0) {
+			while (current.next != null) {
+				current = current.next;
+			}
+		}
+		
+		// 2nd case: delete last index N
+		
+		else if (index == getSize()) {
+			while (current.next != null) {
+				
+			}
+		}
+		
+		// 3rd case: delete in between
+		// TODO fix this case
+		while (current.next != null) {
+			counter++;
+			
+			if (counter == index) {
+				current.next = current.next.next;
+				// for a doubly linked list, current.prev = current.prev.prev
+			}
+			current = current.next;
+		}
+		
+	}
+	
+	
 }
